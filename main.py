4fb121f47836e83c0070e3312a706687815b89ac
gdcraft07 <iklass987654321@gmail.com>
2026-09-12 21:29:38 +0300

    make target. ninja target lmao

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import random
 
 def main():
     pygame.init()
     width, height = (600, 400)
     screen = pygame.display.set_mode((width, height))
 
+    target_radius = 15
+    target_x, target_y = (0, 0)
+    
     done = False
 
     while not done:
@@ -12,7 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
+        target_x, target_y = (random.randint(target_radius, width), random.randint(target_radius, height))
         screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (0, 0, 0), (target_x, target_y), target_radius)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
